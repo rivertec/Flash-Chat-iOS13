479c4675671fc464ca0a3b657f308e241bdb4c19
Garam Kim <oriver1129@gmail.com>
2022-10-10 23:20:34 -0400

    ?

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,8 +14,18 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        titleLabel.text = ""
+        
+        let titleText = "⚡️FlashChat"
+        var charIndex = 0.0
        
+        for char in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(char)
+            }
+            charIndex += 1
+        }
     }
     
 
